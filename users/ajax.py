1577265d2b0cd5847b50dcea0a01d7aeb442af22
Phillip Thelen <viirus@pherth.net>
2012-11-11 22:13:36 +0100

    forgot a template and ajax code for the lend thingy.

---
+++ users/ajax.py
@@ -0,0 +1,15 @@
+from dajax.core import Dajax
+from dajaxice.decorators import dajaxice_register
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+
+@dajaxice_register
+def get_users_to_lend(request, query):
+	dajax = Dajax()
+	userlist = User.objects.filter(username__icontains=query)
+	endlist = []
+	for user in userlist:
+		endlist.append(user.username)
+	dajax.assign('#id_username','data-source', endlist)
+	print dajax.json()
+	return dajax.json()
